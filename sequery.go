b5a5cd93006752e1dbd24ef3cfc0b7a85194c01a
zOldFun <kevfree@mail.ru>
2015-01-23 12:05:35 +0300

    1

--- sequery.go
+++ sequery.go
@@ -10,6 +10,9 @@ import (
 )
 
 func main() {
+
+	log.Println("Starting...")
+
 	resp, err := http.Get("http://yandex.ru/yandsearch?text=%D1%87%D0%B5%D1%82%D0%BA%D0%B8%D0%B5%20%D0%BC%D0%B0%D0%BA%D0%B0%D1%80%D0%BE%D0%BD%D1%8B")
 	if err != nil {
 		//fmt.Printf(err)
@@ -33,7 +36,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//fmt.Printf(string(body))
+	fmt.Printf(string(body))
 	fmt.Printf("hello from golang")
 
 }
